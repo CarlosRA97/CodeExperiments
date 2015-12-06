9181320ba1d894fbc08b804406016cabb6887c09
CarlosRA97 <carlosriveroaro7@gmail.com>
2015-12-06 03:37:00 +0100

    yeah finished but not polished

--- SpyCopy.rb
+++ SpyCopy.rb
@@ -13,17 +13,26 @@ def Directories(p)
 	Dir.entries(p).select {|entry| File.directory? File.join(p,entry) and !(entry =='.' || entry == '..') }
 end
 
+# f es una array de los archivos contenidos en el destino
+
 f = Dir.entries(dest).select {|entry| File.directory? File.join(dest,entry) and !(entry =='.' || entry == '..') }
 
 print f
 
-def check_copy(src,f,dir1)
+def check_copy(src,f,dir1,dest)
 	for folder in f
-		if File.directory?(dir1)
+		if folder == dir1
+			puts 'Se ha encontrado el archivo'
 			break
 		else
-			FileUtils.copy_entry src,f
+			FileUtils.copy_entry src,dest
 		end
+	end
+end
+
+def log_file (s)
+	File.open("logUsb.txt", "a+") do |f|     
+		f.write(s)   
 	end
 end
 
@@ -39,15 +48,12 @@ for dir in Directories(src)
 				print src << '/' << dir1
 				puts ''
 
-				check_copy(src,f,dir1)
-				#File.open("out.txt", "w") do |f|     
-				#	f.write(src)   
-				#end
+				check_copy src,f,dir1,dest
+			else
+				log_file Directories(src)
 			end
 		end
 	else
-		
-		
-		
+		log_file Directories(src)
 	end
 end
