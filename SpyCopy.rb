64feb2fed2107e777d3d0236e31e8020e47f8c46
CarlosRA97 <carlosriveroaro7@gmail.com>
2015-12-06 02:06:24 +0100

    cremita
    creado def para directorios

--- SpyCopy.rb
+++ SpyCopy.rb
@@ -1,45 +1,49 @@
 #!/usr/bin/env ruby
 
-#require 'fileutils'
+require 'fileutils'
 
 #FileUtils.copy_entry , '/home/carlos/Copy'
 
 src = '/media'
-f = '/home/carlos/Escritorio'
+dest = '/home/carlos/Escritorio'
 
 puts src
-puts '1'
 
-Directories = Dir.entries(src).select {|entry| File.directory? File.join(src,entry) and !(entry =='.' || entry == '..') }
+def Directories(p)
+	Dir.entries(p).select {|entry| File.directory? File.join(p,entry) and !(entry =='.' || entry == '..') }
+end
 
-for dir in Directories
+f = Dir.entries(dest).select {|entry| File.directory? File.join(dest,entry) and !(entry =='.' || entry == '..') }
+
+def check_copy(src,f,dir1)
+	for folder in f
+		if folder == dir1
+			break
+		else
+			FileUtils.copy_entry src,f
+		end
+	end
+end
+
+for dir in Directories(src)
 
 	if dir == 'ESD-USB'
 		print src << '/' << dir
 		puts ''
 
-		Directories = Dir.entries(src).select {|entry| File.directory? File.join(src,entry) and !(entry =='.' || entry == '..') }
-		print Directories
-
-		puts ''
-		for dir1 in Directories
+		for dir1 in Directories(src)
 
 			if dir1 == 'Copy'
 				print src << '/' << dir1
 				puts ''
 
-				Directories = Dir.entries(src).select {|entry| File.directory? File.join(src,entry) and !(entry =='.' || entry == '..') }
-				print Directories
-
-				puts ''
+				check_copy(src,f,dir1)
 				#File.open("out.txt", "w") do |f|     
 				#	f.write(src)   
 				#end
 			end
 		end
+	else
+		
 	end
 end
-
-	
-
-#FileUtils.copy_entry src,f
