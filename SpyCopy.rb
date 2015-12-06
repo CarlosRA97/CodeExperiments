c24adf9991b081d74f1112ced8dab78f8a3b97e0
Carlos Rivero Aro <carlosriveroaro7@gmail.com>
2015-12-06 01:03:16 +0100

    Update SpyCopy.rb

--- SpyCopy.rb
+++ SpyCopy.rb
@@ -1,24 +1,40 @@
 #!/usr/bin/env ruby
 
-#require 'fileutils'
+require 'fileutils'
 
 #FileUtils.copy_entry , '/home/carlos/Copy'
 
 src = '/media'
+f = '/home/carlos/Escritorio'
+
+puts src
+puts '1'
 
 Directories = Dir.entries(src).select {|entry| File.directory? File.join(src,entry) and !(entry =='.' || entry == '..') }
 
 for dir in Directories
-	if dir == '/ESD-USB'
-		src << dir
-		dir1 = Dir.entries(src).select {|entry| File.directory? File.join(src,entry) and !(entry =='.' || entry == '..') }
-		for dir2 in dir1
-			if dir2 == '/Copy'
-				src << dir2
-				dir3 = Dir.entries(src).select {|entry| File.directory? File.join(src,entry) and !(entry =='.' || entry == '..') }
-			end
-		end
-	else
-		File.new 
-		File.open("Susb.txt", 'a') {|f| f.write(dir) } 
+
+	if dir == 'ESD-USB'
+		print src << '/' << dir
+		puts ''
+
+		$dir1 = Dir.entries(src).select {|entry| File.directory? File.join(src,entry) and !(entry =='.' || entry == '..') }
+
+		print dir1
+		puts ''
+	end
 end
+
+for dir2 in $dir1
+	if $dir1 == 'Copy'
+		print src << '/' << dir2
+		puts '3'
+		dir3 = Dir.entries(src).select {|entry| File.directory? File.join(src,entry) and !(entry =='.' || entry == '..') }
+		File.open("out.txt", "w") do |f|     
+			f.write(src)   
+		end
+	end
+end
+	
+
+#FileUtils.copy_entry src,f
