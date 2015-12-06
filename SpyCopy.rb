9e2c16b675638a2564db8ee67f2be35d1dcd362e
CarlosRA97 <carlosriveroaro7@gmail.com>
2015-12-06 04:14:45 +0100

    update

--- SpyCopy.rb
+++ SpyCopy.rb
@@ -5,8 +5,6 @@ require 'fileutils'
 src = '/media'
 dest = '/home/carlos/Escritorio'
 
-puts src
-
 def Directories(p)
 	Dir.entries(p).select {|entry| File.directory? File.join(p,entry) and !(entry =='.' || entry == '..') }
 end
@@ -15,43 +13,39 @@ end
 
 f = Dir.entries(dest).select {|entry| File.directory? File.join(dest,entry) and !(entry =='.' || entry == '..') }
 
-print f
-
 def check_copy(src,f,dir1,dest)
 	for folder in f
 		if folder == dir1
 			puts 'Se ha encontrado el archivo'
 			break
-		else
+			else
 			FileUtils.copy_entry src,dest
 		end
 	end
 end
 
 def log_file (s)
-	File.open("logUsb.txt", "a+") do |f|     
-		f.write(s)   
+	File.open("logUsb.txt", "a+") do |f|
+		f.write(s)
 	end
 end
 
 for dir in Directories(src)
-
-	if dir == 'ESD-USB'
-		print src << '/' << dir
-		puts ''
-
+	
+	if dir == 'ESD-USB' and
+		src << '/' << dir
+		
 		for dir1 in Directories(src)
-
+			
 			if dir1 == 'Copy'
-				print src << '/' << dir1
-				puts ''
-
+				src << '/' << dir1
+				
 				check_copy src,f,dir1,dest
-			else
-				log_file Directories(src)
+				else
+				log_file dir1 + " | "
 			end
 		end
-	else
-		log_file Directories(src)
+		else
+		log_file dir + " | "
 	end
 end
