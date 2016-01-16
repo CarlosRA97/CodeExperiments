f1dde21a6552a159f060f8b98b07c94af19e4176
CarlosRA97 <carlosriveroaro7@gmail.com>
2016-01-16 20:02:53 +0100

    Passing code to class mode

--- sha1gen/sha1genBase.py
+++ sha1gen/sha1genBase.py
@@ -7,24 +7,32 @@ from os import system
 
 #   Encripta lo el usuario introduce y luego
 #   copia en el portapapeles el resultado
-def sha1_digest(key):
-    
-    h = hashlib.sha1(key).hexdigest()
-    
-    if platform.startswith('darwin') or platform.startswith('linux'):
-        cp2clipb(h)
-    elif platform.startswith('win32'):
-        copy2clip(h)
 
-def copy2clip(txt):
-    
-    cmd='echo '+txt.strip()+'|clip'
-    
-    return subprocess.check_call(cmd, shell=True)
+class CopyToClipboard(object):
+    def copy2clip(txt):
 
-def cp2clipb(txt):
-    
-    return system("echo '%s' | pbcopy" % txt)
+        cmd='echo '+txt.strip()+'|clip'
+
+        return subprocess.check_call(cmd, shell=True)
+
+    def cp2clipb(txt):
+
+        return system("echo '%s' | pbcopy" % txt)
+
+
+class Encrypt(CopyToClipboard):
+    def sha1_digest(key):
+
+        h = hashlib.sha1(key).hexdigest()
+
+        if platform.startswith('darwin') or platform.startswith('linux'):
+            cp2clipb(h)
+        elif platform.startswith('win32'):
+            copy2clip(h)
+
+
 
 #Llamamos a la funcion para hacer el hash
-sha1_digest(raw_input("Introducee un texto: "))
+
+key = Encrypt()
+key.sha1_digest(raw_input("Escribe el texto: "))
